a3616a2f1fa912b8b50ff6073bd488d7638ab3c6
Elliot Chance <elliotchance@gmail.com>
2020-08-29 16:24:02 -0400

    Standard library is now committed to git
    Previously, the generated vm/lib.go (containing the compiled standard
    library) replied on the Makefile generating it. This means that people
    that pulled in the ok package would not have any of the standard library
    built.
    
    It's unfortunate that the generated code needs to be committed, so I've
    made some cosmetic changes to the rendered code to bring the ~4000 lines
    to about 1300 lines. So at least it's more git diff friendly.

---
+++ vm/render.go
@@ -0,0 +1,155 @@
+package vm
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/elliotchance/ok/ast"
+)
+
+func Render(f io.Writer, x interface{}, indent string, vmPackage bool) {
+	ty := reflect.TypeOf(x)
+	switch ty.Kind() {
+	case reflect.Map:
+		renderMap(f, x, indent, vmPackage)
+
+	case reflect.Ptr:
+		renderPtr(f, x, indent, vmPackage)
+
+	case reflect.Slice:
+		renderSlice(f, x, indent, vmPackage)
+
+	default:
+		fmt.Fprintf(f, "%#v", x)
+	}
+}
+
+func localType(x interface{}, vmPackage bool) string {
+	if vmPackage {
+		return strings.Replace(fmt.Sprintf("%T", x), "vm.", "", -1)
+	}
+
+	return fmt.Sprintf("%T", x)
+}
+
+func renderMap(f io.Writer, x interface{}, indent string, vmPackage bool) {
+	v := reflect.ValueOf(x)
+	if len(v.MapKeys()) == 0 {
+		fmt.Fprintf(f, "nil")
+		return
+	}
+
+	// Maps keys need to be sorted.
+	var mapKeys []reflect.Value
+	for _, key := range v.MapKeys() {
+		mapKeys = append(mapKeys, key)
+	}
+
+	sort.Slice(mapKeys, func(i, j int) bool {
+		return mapKeys[i].String() < mapKeys[j].String()
+	})
+
+	fmt.Fprintf(f, "%s{\n", localType(x, vmPackage))
+	for _, key := range mapKeys {
+		fmt.Fprintf(f, "%s\t\"%s\": ", indent, key)
+		Render(f, v.MapIndex(key).Interface(), indent+"\t", vmPackage)
+		fmt.Fprintf(f, ",\n")
+	}
+	fmt.Fprintf(f, "%s}", indent)
+}
+
+func renderSlice(f io.Writer, x interface{}, indent string, vmPackage bool) {
+	// Some types are handled as a single line. This is so the output generated
+	// for vm/lib.go is far less verbose and more version control friendly.
+	switch x.(type) {
+	case Registers, []string:
+		renderSliceOneLine(f, x, indent, vmPackage)
+		return
+	}
+
+	v := reflect.ValueOf(x)
+	if v.Len() == 0 {
+		fmt.Fprintf(f, "nil")
+		return
+	}
+
+	fmt.Fprintf(f, "%s{\n", localType(x, vmPackage))
+	for i := 0; i < v.Len(); i++ {
+		value := v.Index(i)
+		if !value.IsZero() {
+			fmt.Fprintf(f, "%s\t", indent)
+			Render(f, value.Interface(), indent+"\t", vmPackage)
+			fmt.Fprintf(f, ",\n")
+		}
+	}
+	fmt.Fprintf(f, "%s}", indent)
+}
+
+func renderSliceOneLine(f io.Writer, x interface{}, indent string, vmPackage bool) {
+	v := reflect.ValueOf(x)
+	if v.Len() == 0 {
+		fmt.Fprintf(f, "nil")
+		return
+	}
+
+	fmt.Fprintf(f, "%s{", localType(x, vmPackage))
+	for i := 0; i < v.Len(); i++ {
+		if i > 0 {
+			fmt.Fprintf(f, ",")
+		}
+		value := v.Index(i)
+		Render(f, value.Interface(), indent+"\t", vmPackage)
+	}
+	fmt.Fprintf(f, "}")
+}
+
+func renderPtr(f io.Writer, x interface{}, indent string, vmPackage bool) {
+	// Some types are handled as a single line. This is so the output generated
+	// for vm/lib.go is far less verbose and more version control friendly.
+	switch x.(type) {
+	case Instruction, *ast.Literal, *ast.Argument:
+		renderPtrWithoutKeys(f, x, indent, vmPackage)
+		return
+	}
+
+	v := reflect.ValueOf(x).Elem()
+	ty := reflect.TypeOf(x).Elem()
+
+	if !v.IsValid() {
+		fmt.Fprintf(f, "nil")
+		return
+	}
+
+	fmt.Fprintf(f, "&%s{\n", localType(x, vmPackage)[1:])
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if !field.IsZero() {
+			fmt.Fprintf(f, "%s\t%s: ", indent, ty.Field(i).Name)
+			Render(f, field.Interface(), indent+"\t", vmPackage)
+			fmt.Fprintf(f, ",\n")
+		}
+	}
+	fmt.Fprintf(f, "%s}", indent)
+}
+
+func renderPtrWithoutKeys(f io.Writer, x interface{}, indent string, vmPackage bool) {
+	v := reflect.ValueOf(x).Elem()
+
+	if !v.IsValid() {
+		fmt.Fprintf(f, "nil")
+		return
+	}
+
+	fmt.Fprintf(f, "&%s{", localType(x, vmPackage)[1:])
+	for i := 0; i < v.NumField(); i++ {
+		if i > 0 {
+			fmt.Fprintf(f, ", ")
+		}
+		field := v.Field(i)
+		Render(f, field.Interface(), indent+"\t", vmPackage)
+	}
+	fmt.Fprintf(f, "}")
+}
